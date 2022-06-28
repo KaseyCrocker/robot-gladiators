9d7215f14bbc8d0bb58a88c3ae8e24d64f93e1c1
Kasey <kasey_crocker@yahoo.com>
2022-06-28 17:25:08 -0400

    changed shop input

--- assets/js/game.js
+++ assets/js/game.js
@@ -149,31 +149,25 @@ var startGame = function() {
 var shop = function() {
   // ask player what they'd like to do
   var shopOptionPrompt = window.prompt(
-    "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one: 'REFILL', 'UPGRADE', or 'LEAVE' to make a choice."
-  )
+    "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one 1 for REFILL, 2 for UPGRADE, or 3 for LEAVE."
+  );
+  shopOptionPrompt = parseInt(shopOptionPrompt);
     // use switch to carry out action
     switch (shopOptionPrompt) {
-      case "REFILL":
-      case "refill":
-        playerInfo.refillHealth();
-        break;
-      case "UPGRADE":
-      case "upgrade":
-        playerInfo.upgradeAttck();
-        break;
-      case "LEAVE":
-      case "leave":
-        window.alert("Leaving the store.");
-    
-        // do nothing, so function will end
-        break;
-      default:
-        window.alert("You did not pick a valid option. Try again.");
-    
-        // call shop() again to force player to pick a valid option
-        shop();
-        break;
-    }
+    case 1:
+      playerInfo.refillHealth();
+      break;
+    case 2:
+      playerInfo.upgradeAttack();
+      break;
+    case 3:
+      window.alert("Leaving the store.");
+      break;
+    default:
+      window.alert("You did not pick a valid option. Try again.");
+      shop();
+      break;
+  }
 };
 
 // function to generate a random numeric value
@@ -215,7 +209,7 @@ var playerInfo = {
       window.alert("you don't have enough money!");
     }
   },
-  upgradeAttck: function() {
+  upgradeAttack: function() {
     if (this.money >=7) {
       window.alert("upgrading player's attck by 6 for 7 dollars!");
       this.attack += 6;
